14d4f6fec0cfe2e713855376c938d0ae0ef98b66
edoardorossi3 <edoardo.rossi3@stud.unifi.it>
2021-07-14 17:28:34 +0200

    spectre 14/7 17:29

--- tesi_py/plot_spec.py
+++ tesi_py/plot_spec.py
@@ -17,7 +17,8 @@ def spectre(suffix_file, age50_min, age50_max):
     import function_plot as f_plt
     ## generate color vector to be used in plot (color=c)
     
-    work_dir=os.getenv('HOME')+'/Desktop/TESI/models/Sandage_v4.1_Zfix_noburst_cb16MILES_1M/'
+    #work_dir=os.getenv('HOME')+'/Desktop/TESI/models/Sandage_v4.1_Zfix_noburst_cb16MILES_1M/'
+    work_dir='/export/home/extragal/zibetti/no_ownCloud/SteMaGE/data/SEDlibraries/Sandage_v4.1_Zfix_noburst_cb16MILES_1M/'
     #name_file='sandage_varZ_v4.1_m62fix_noburst_1M_spec_dcombnull_001.fits'
     snr='200'
     file_spec=work_dir+suffix_file+'_001.fits'
@@ -31,7 +32,6 @@ def spectre(suffix_file, age50_min, age50_max):
     hdul_pert=fits.open(file_pert)
     
     wl=hdul[0].data
-    spec=hdul[1].data
     age10=hdul_par[1].data['age10']
     age50=hdul_par[1].data['age50']
     age90=hdul_par[1].data['age90']
@@ -47,6 +47,64 @@ def spectre(suffix_file, age50_min, age50_max):
     i=hdul_pert[1].data['ABMAG_I_PERT']
     z=hdul_pert[1].data['ABMAG_Z_PERT']
     
+    #starting spectra
+    print('age50:', age50[0])  
+    #spec_sel=np.arange(np.size(hdul[1].data[0,...]))
+    #for j in range(0, np.size(age50)):
+    #    if ((np.log10(age50[j])<age50_max) & (np.log10(age50[j])>age50_min)):
+    #        spec_sel=np.array([spec_sel, hdul[1].data[j,...]])
+    
+    print('checkpoint 1')
+    #print('shape spec:', np.shape(spec_sel))
+    
+    for i_chunk in range (2,51):
+        _file_spec=work_dir+suffix_file+'_{:03d}.fits'
+        _file_par=work_dir+suffix_file+'_{:03d}_physpar_wagef.fits'
+        _file_pert=work_dir+suffix_file+'_perterr_SNR'+snr+'_{:03d}.fits'
+
+        _hdul_par=fits.open(_file_par.format(i_chunk))
+        _hdul_pert=fits.open(_file_pert.format(i_chunk))
+        
+        
+        _age10=_hdul_par[1].data['age10']
+        _age50=_hdul_par[1].data['age50']
+        _age90=_hdul_par[1].data['age90']
+        _d4000n=_hdul_pert[1].data['D4000N_SIGMA200_PERT']
+        _hdhg=_hdul_pert[1].data['HDHG_SIGMA200_PERT']
+        _hb=_hdul_pert[1].data['LICK_HB_SIGMA200_PERT']
+        _mg2fe=_hdul_pert[1].data['MG2FE_SIGMA200_PERT']
+        _mgfep=_hdul_pert[1].data['MGFE_PRIME_SIGMA200_PERT']
+        _u=_hdul_pert[1].data['ABMAG_U_PERT']
+        _g=_hdul_pert[1].data['ABMAG_G_PERT']
+        _r=_hdul_pert[1].data['ABMAG_R_PERT']
+        _i=_hdul_pert[1].data['ABMAG_I_PERT']
+        _z=_hdul_pert[1].data['ABMAG_Z_PERT']
+        
+        #for k in range(0, np.size(_age50)):
+        #    if ((np.log10(_age50[k])<age50_max) & (np.log10(_age50[k])>age50_min)):
+        #        spec_sel=np.array([spec_sel, _hdul_spec[1].data[k,...]])
+        
+        age50=np.append(age50, _age50)
+        age10=np.append(age10, _age10)
+        age90=np.append(age90, _age90)
+        d4000n=np.append(d4000n, _d4000n)
+        hdhg=np.append(hdhg, _hdhg)
+        hb=np.append(hb, _hb)
+        mg2fe=np.append(mg2fe, _mg2fe)
+        mgfep=np.append(mgfep, _mgfep)
+        u=np.append(u, _u)
+        g=np.append(g, _g)
+        r=np.append(r, _r)
+        i=np.append(i, _i)
+        z=np.append(z, _z)
+        
+    
+    sel_models=((np.log10(age50)<age50_max) & (np.log10(age50)>age50_min))
+    N_row=np.sum(sel_models)
+    N_col=np.size(hdul[1].data[0,...])
+    matrix_spec=np.zeros(shape=(N_row, N_col))
+    #spec_sel=np.delete(spec_sel, 0, 0)  #delete the first row    
+    
     n_bin=int((age50_min-6.0)/0.05)
     N_models=np.size(age10)
     idx_array=np.arange(N_models)
@@ -60,9 +118,9 @@ def spectre(suffix_file, age50_min, age50_max):
     sel_wl=((wl>wl_range[0]) & (wl<wl_range[1]))
     sel_norm=((wl>wl_norm[0]) & (wl<wl_norm[1]))
         
-    sel_models=((np.log10(age50)>age50_min) & (np.log10(age50)<age50_max))
-    print(np.size(sel_models))
-    print(np.sum(sel_models))
+    #sel_models=((np.log10(age50)>age50_min) & (np.log10(age50)<age50_max))
+    #print(np.size(sel_models))
+    #print(np.sum(sel_models))
     sel_ref=((np.log10(age50)>age50_min) & (np.log10(age50)<age50_max)&(dage_norm<-1.0))
     idx_sel=idx_array[sel_models]
     widths=[5,0.05]
@@ -70,20 +128,41 @@ def spectre(suffix_file, age50_min, age50_max):
     
     gs=dict(width_ratios=widths, height_ratios=heights)
     fig, axs=plt.subplots(2,2,figsize=(40,10), gridspec_kw=gs)
+    print('size r:', np.size(r))
+    print('size i:', np.size(i))
 
     delta=f_plt.chi_q(dage_norm,d4000n, hdhg, hb, mg2fe, mgfep, u-r,g-r, r-i, r-z, sel_models, sel_ref,mkplot=False, sigma_obs=False)
+    first_row=0
+    for i_chunk in range(0,50):
+        _file_spec=work_dir+suffix_file+'_{:03d}.fits'
+        _hdul_spec=fits.open(_file_spec.format(i_chunk+1))
+        _sel_mod=sel_models[i_chunk*20000:(i_chunk+1)*20000]
+        _spec=_hdul_spec[1].data
+        last_row=np.sum(_sel_mod)+first_row
+        matrix_spec[first_row:last_row,...]=_spec[_sel_mod,...]
+        
+        print('first_row:', first_row,'last_row:', last_row)
+        first_row=last_row
     
-    spec_sel=spec[sel_models, ...]
-    sel_delta_min=(delta==np.min(delta))
-    norm=np.mean(spec_sel[sel_delta_min,sel_norm])
+    print('n_row:', N_row)
+    #return
+    #spec_sel=spec[sel_models, ...]
+    #sel_delta_min=(delta==np.min(delta))
+    arg_delta_min=np.argmin(delta)
+    #print('first spec:', np.shape(hdul[1].data[0,...]))
+    #norm=np.mean(matrix_spec[sel_delta_min,sel_norm])
+    norm=np.mean(matrix_spec[arg_delta_min,sel_norm])
     #axs[0,0].plot(wl[sel_wl], spec_sel[sel_delta_min, sel_wl]/norm, color='black')
     
-    for i_model in range(0, np.size(idx_sel)):
-        _norm=np.mean(spec[idx_sel[i_model],sel_norm])
+    for i_model in range(0, N_row):
+        #_norm=np.mean(spec[idx_sel[i_model],sel_norm])
+        _norm=np.mean(matrix_spec[i_model, sel_norm])
         c=cm.rainbow_r((dage_norm[idx_sel[i_model]]-dage_min)/(dage_max-dage_min))
-        axs[0,0].plot(wl[sel_wl], spec[idx_sel[i_model], sel_wl]/_norm, alpha = 0.1,linewidth = 0.1, color=c)
-        axs[1,0].plot(wl[sel_wl], spec[idx_sel[i_model], sel_wl]/_norm-spec_sel[sel_delta_min, sel_wl]/norm, alpha = 0.1,linewidth = 0.1, color=c)
-
+        #axs[0,0].plot(wl[sel_wl], spec[idx_sel[i_model], sel_wl]/_norm, alpha = 0.1,linewidth = 0.1, color=c)
+        #axs[1,0].plot(wl[sel_wl], spec[idx_sel[i_model], sel_wl]/_norm-spec_sel[sel_delta_min, sel_wl]/norm, alpha = 0.1,linewidth = 0.1, color=c)
+        axs[0,0].plot(wl[sel_wl], matrix_spec[i_model, sel_wl]/_norm, alpha = 0.1,linewidth = 0.1, color=c)
+        axs[1,0].plot(wl[sel_wl], matrix_spec[i_model, sel_wl]/_norm-matrix_spec[arg_delta_min, sel_wl]/norm, alpha = 0.1,linewidth = 0.1, color=c)
+        
     
     NORM=Normalize(vmin=dage_min, vmax=dage_max)
     clb.ColorbarBase(axs[0,1], cmap=cm.rainbow_r, norm=NORM)
